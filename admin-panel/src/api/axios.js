cdd578cdea6779819de8d3fd03a991c504f21351
VISHNUGATTU <gattuvishnuphani@gmail.com>
2026-09-23 18:36:19 +0530

    Final commit 23rdSep

--- admin-panel/src/api/axios.js
+++ admin-panel/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:6446/api', // Connect directly to backend
-  withCredentials: true, // Important for HTTP-only cookies
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
